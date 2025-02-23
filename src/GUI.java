81d016c13d6476d5a67cfeace27eedd08e5df59f
Spyke <matty.lim718@gmail.com>
2025-02-23 21:11:00 +0800

    Yessir

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI() {
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
